2002a815e992bbce62440aed1212dd1afa8f662e
BlueHaven1 <havenuebl@gmail.com>
2025-05-21 01:58:23 +0000

    Update

--- config/supabase.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Supabase Configuration
- */
-return [
-    'url' => env('SUPABASE_URL', ''),
-    'key' => env('SUPABASE_KEY', ''),
-    'jwt_secret' => env('SUPABASE_JWT_SECRET', ''),
-];
